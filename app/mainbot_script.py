d89153a67f4e7e0796f078bdeeee33b47ff14d9e
BankaHooks <bankahookswork@gmail.com>
2026-07-31 12:23:08 +0400

    Adding basic Telegram bot script

--- app/mainbot_script.py
+++ app/mainbot_script.py
@@ -9,4 +9,22 @@ def send_welcome(message):
     welcome_text = f"Hello , {message.from_user.first_name} , welcome to MuseTwinDate!"
     bot.send_message(chat_id=message.chat.id, text=welcome_text)
 
+@bot.message_handler(commands=['help'])
+def send_help(message):
+    list_of_commands = {
+        "/start" : "Welcome to user",
+        "/help" : "Describe every commands to user",
+        "/app" : "Starting application",
+        "/premium" : "Here you can buy premium status",
+    }
+    help_menu = f"Here is list of commands: {[key for key,value in list_of_commands.items()]}"
+    bot.send_message(chat_id=message.chat.id, text=help_menu)
+
+@bot.message_handler(commands=['premium'])
+def premium(message):
+    list_of_premium = {
+
+    }
+
 bot.polling()
+
