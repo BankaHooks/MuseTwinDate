e039d89c31890038f824aed9dedd9b6ba233e2f8
BankaHooks <bankahookswork@gmail.com>
2026-08-15 23:34:22 +0400

    fixing bot bug

--- bot/database/models.py
+++ bot/database/models.py
@@ -1,12 +1,12 @@
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, ForeignKey, Text
+from sqlalchemy import Column, BigInteger, Integer, String, Boolean, DateTime, ForeignKey, Text
 from sqlalchemy.orm import relationship
 from .db import Base
 
 class User(Base):
     __tablename__ = "users"
     id = Column(Integer, primary_key=True)
-    telegram_id = Column(Integer, unique=True, nullable=False)
+    telegram_id = Column(BigInteger, unique=True, nullable=False)
     username = Column(String(255), nullable=True)
     name = Column(String(255), nullable=True)
     gender = Column(String(20), nullable=True)
