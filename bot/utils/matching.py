b2751ba9d3050e55519f73492801ec00adb6a91b
BankaHooks <bankahookswork@gmail.com>
2026-08-16 19:55:49 +0400

    new patch

--- bot/utils/matching.py
+++ bot/utils/matching.py
@@ -7,19 +7,20 @@ from utils.helpers import parse_comma_separated, normalize_goal
 
 logger = logging.getLogger(__name__)
 
+# Веса для расчёта совпадения (сумма = 1.0)
 WEIGHT_SONGS = 0.30
 WEIGHT_BANDS = 0.25
-WEIGHT_GENRES = 0.25
+WEIGHT_GENRES = 0.20
 WEIGHT_GAMES = 0.10
 WEIGHT_INTERESTS = 0.10
-WEIGHT_GOAL = 0.10
+WEIGHT_GOAL = 0.05
 
-def jaccard_similarity(set1: set, set2: set) -> float:
+def sorensen_similarity(set1: set, set2: set) -> float:
+    """Коэффициент Сёренсена (2*|пересечение| / (|set1|+|set2|))"""
     if not set1 or not set2:
         return 0.0
     intersection = len(set1 & set2)
-    union = len(set1 | set2)
-    return intersection / union if union > 0 else 0.0
+    return 2 * intersection / (len(set1) + len(set2))
 
 def calculate_match_score(user1: User, user2: User) -> float:
     songs1 = parse_comma_separated(user1.favorite_songs)
@@ -33,11 +34,11 @@ def calculate_match_score(user1: User, user2: User) -> float:
     interests1 = parse_comma_separated(user1.interests)
     interests2 = parse_comma_separated(user2.interests)
 
-    songs_score = jaccard_similarity(songs1, songs2)
-    bands_score = jaccard_similarity(bands1, bands2)
-    genres_score = jaccard_similarity(genres1, genres2)
-    games_score = jaccard_similarity(games1, games2)
-    interests_score = jaccard_similarity(interests1, interests2)
+    songs_score = sorensen_similarity(songs1, songs2)
+    bands_score = sorensen_similarity(bands1, bands2)
+    genres_score = sorensen_similarity(genres1, genres2)
+    games_score = sorensen_similarity(games1, games2)
+    interests_score = sorensen_similarity(interests1, interests2)
 
     goal1 = normalize_goal(user1.search_goal)
     goal2 = normalize_goal(user2.search_goal)
