2901da37db6e66e113c0ff9833ade86df0bd29d5
BankaHooks <bankahookswork@gmail.com>
2026-08-16 16:07:06 +0400

    referal system added

--- bot/states/registration.py
+++ bot/states/registration.py
@@ -1,6 +1,6 @@
 from aiogram.fsm.state import State, StatesGroup
 
-class Registration(StatesGroup):
+class RegistrationState(StatesGroup):
     name = State()
     gender = State()
     age = State()
@@ -8,6 +8,8 @@ class Registration(StatesGroup):
     genres = State()
     bands = State()
     songs = State()
+    albums = State()
+    artists = State()
     goal = State()
     interests = State()
     preferred_gender = State()
